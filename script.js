c89a40b7648062e9a213801a15ae26794badb254
rinajsmn <rina.jsmn0325@gmail.com>
2024-08-22 20:41:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll('.question');
+
+function toggleAccordion(){
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
